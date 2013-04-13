6f342bb64ac7c453b5de398590b028ec59375b44
Erik Dohnberg <erikdohnberg@gmail.com>
2013-04-13 15:09:18 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bitquery::Application.config.secret_token = '9523fa08498d102b6ef218d31d879ab9bbe4353b6761ce7e718653a5aacfd212d7f7a91dd7d754047ea33e5a69148341127d29bf389c2b5a8193ae27d8c96771'
