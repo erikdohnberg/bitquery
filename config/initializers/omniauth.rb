7670d172b2812123636a913a59074cebb6ff47e1
Erik Dohnberg <erikdohnberg@gmail.com>
2013-05-10 13:54:51 -0400

    Fixing security key problem with omniauth. Resetting twitter keys as a result.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'Ku9aIHZepkV7vgKnBQTd0w', 'rqiq7oLg3eCZL40gi6KFpYIyVHnl8aE9mOfIavQkgVw'
+  provider :twitter, 'TWITTER_CONSUMER_KEY', 'TWITTER_CONSUMER_SECRET'
 end
