d63fbcfda61bb2778b83b97786bc64599958c2b7
Erik Dohnberg <erikdohnberg@gmail.com>
2013-05-21 21:01:55 -0400

    Dropping unnecessary instructors table from database

--- db/schema.rb
+++ db/schema.rb
@@ -11,21 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130414034427) do
-
-  create_table "instructors", :force => true do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "thandle"
-    t.string   "crypted_password"
-    t.string   "salt"
-    t.datetime "created_at",                   :null => false
-    t.datetime "updated_at",                   :null => false
-    t.string   "remember_me_token"
-    t.datetime "remember_me_token_expires_at"
-  end
-
-  add_index "instructors", ["remember_me_token"], :name => "index_users_on_remember_me_token"
+ActiveRecord::Schema.define(:version => 20130522004935) do
 
   create_table "tweets", :force => true do |t|
     t.datetime "created_at",  :null => false
